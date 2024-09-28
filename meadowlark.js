3c0f11979f5b91f07e159e36b3402c228f5008f8
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-09-29 00:09:57 +0100

    testing

--- meadowlark.js
+++ meadowlark.js
@@ -2,25 +2,29 @@ const express = require('express')
 const port = process.env.PORT || 5000
 const {engine} = require('express-handlebars')
 const fortune = require('./lib/fortune')
+const { about, home, serverError, notfound } = require('./lib/handlers')
 
 const app = express()
 
-
-    
 app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => {
-    res.render('home')
+app.get('/', home)
+app.get('/headers', (req, res) => {
+    res.type('text/plain')
+    const headers = Object.entries(req.headers)
+        .map(([key, value]) => `${key}: ${value}`)
+    // res.send(headers.join('\n'))
+    res.send(req.url)
 })
-app.get('/about', (req, res) => {
-    
-    res.render('about', {fortune: fortune.getFortune()})
-})
-app.use((req, res) => {
-    res.status(404)
-    res.render('404')
-})
-.listen(port)
+app.get('/about', about)
+app.use(notfound)
+if (require.main === module) {
+    app.listen(port, () => {
+        console.log(`Express started on http://localhost:${port}`)
+    })
+} else {
+    module.exports = app
+}
